fa494936f4c687f9e3105f26b78d1f3ba9968d35
Asenyka <humsterchik@yandex.ru>
2023-07-06 11:20:32 +0300

    added required email in routes patch/me

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,8 @@ router.get('/me', getCurrentUser);
 
 router.patch('/me', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().regex(RegExp(EMAIL_REGEX)),
-    name: Joi.string().min(2).max(30),
+    email: Joi.string().required().regex(RegExp(EMAIL_REGEX)),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), updateUser);
 
